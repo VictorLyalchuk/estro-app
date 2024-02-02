1b881435598c2cb37c8a0f0ec5dcc2bd1f647726
VictorLyalchuk <121404620+VictorLyalchuk@users.noreply.github.com>
2024-02-02 20:09:46 +0200

    fix

--- src/components/Product/AddProduct.tsx
+++ src/components/Product/AddProduct.tsx
@@ -204,6 +204,8 @@ const AddProduct = () => {
                                             type="name"
                                             autoComplete="name"
                                             placeholder="Enter name"
+                                            required
+
                                         // className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
                                     </Form.Item>
@@ -223,6 +225,8 @@ const AddProduct = () => {
                                             type="text"
                                             autoComplete="article"
                                             placeholder="Enter article"
+                                            required
+
                                         // className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
                                     </Form.Item >
@@ -244,6 +248,7 @@ const AddProduct = () => {
                                             autoComplete="price"
                                             placeholder="0.00"
                                             addonBefore="₴"
+                                            required
                                         // className="w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
                                     </Form.Item>
