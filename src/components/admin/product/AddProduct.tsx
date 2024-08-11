dda6ad73c76336671c877ff1fb2c73ef9da27b65
VictorLyalchuk <121404620+VictorLyalchuk@users.noreply.github.com>
2024-08-12 01:27:54 +0300

    1

--- src/components/admin/product/AddProduct.tsx
+++ src/components/admin/product/AddProduct.tsx
@@ -577,7 +577,6 @@ const AddProduct = () => {
                                     </button>
                                 </div>
                             </form>
-                            {/* </Form> */}
                         </div>
                     </div>
                 </div>
