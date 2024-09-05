83efde30a480ae13b35c630a4e69e368a548b47e
VictorLyalchuk <121404620+VictorLyalchuk@users.noreply.github.com>
2024-09-06 00:18:59 +0300

    erter

--- src/components/user/product/ProductQuickview.tsx
+++ src/components/user/product/ProductQuickview.tsx
@@ -159,7 +159,7 @@ const ProductQuickview: React.FC<IProductQuickviewProps> = ({ product, isOpen, s
                                 leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
                             >
                                 <Dialog.Panel className="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
-                                    <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14  sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                                    <div className="relative flex w-full items-center overflow-hidden bg-gray-100 px-4 pb-8 pt-14  sm:px-6 sm:pt-8 md:p-6 lg:p-8">
                                         <button
                                             type="button"
                                             className="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8"
